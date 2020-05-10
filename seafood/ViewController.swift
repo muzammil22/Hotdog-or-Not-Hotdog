b512e727ba60901de745bf4d6aafc3f128be0c57
muzammil22 <muzzammilmuneer@gmail.com>
2020-05-10 21:22:01 +0500

    Initial Commit

---
+++ seafood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  seafood
+//
+//  Created by Muzammil Muneer on 10/05/2020.
+//  Copyright © 2020 Muzammil Muneer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
